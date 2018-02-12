3e673c57f2ef8c87a0bb782552b7a5f973c467a2
Christopher Vicuña <christopher.vicuna@gmail.com>
2018-02-12 00:02:21 -0300

    probando componentes

---
+++ componentes/main.js
@@ -0,0 +1,30 @@
+Vue.component('videoclub',{
+    props: ['image','title'],
+    template: `
+        <div>
+            <img width="100" :src="image" :alt="title" />
+            <h2>{{title}}</h2>
+        </div>
+    `
+})
+
+const app = new Vue({
+    el: '#app',
+    data: { 
+        movies: [
+            {title:'Matrix', img: 'https://lh3.googleusercontent.com/TQp6Lb2rArWLcWaUkXyBfecjm8DWrk00rbpU_Z5h3Xe6RNC2Oc3QfHCTi96tud31urnYUeK027Ft' },
+            {title:'5to Elemento', img: 'https://imgpv.com/images/2014/04/04/elquintoelemento19971.jpg' },
+            {title:'Alien Isolation', img: 'http://deeragaming.net/wp-content/uploads/2014/03/alien-isolation-wallpaper.jpg' }
+        ]
+    },
+    template: `
+    <div>
+        <videoclub
+            v-for="(movie, index) in movies"
+            :key="index"
+            :title="movie.title"
+            :image="movie.img"
+        ></videoclub>
+    </div>
+    `
+})
